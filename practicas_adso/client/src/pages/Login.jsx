d0a1b943d63e5d9ae5bb5d8caeb8e0206e9f2266
santifloroja18 <133369865+santifloroja18@users.noreply.github.com>
2023-11-07 12:48:32 -0500

    Add files via upload

--- practicas_adso/client/src/pages/Login.jsx
+++ practicas_adso/client/src/pages/Login.jsx
@@ -2,20 +2,39 @@ import '../styles/Login.css'
 
 export default function Login(){
     return(
-        <div>
-            <div></div>
-            <div id='cuadrado'>
-                <img src="../../../logoSena.jpeg" alt="SENALOVER" />
-                <h1>INICIA SESIÓN</h1>
-                <form action="" method="post">
-                    <label htmlFor="email">CORREO ELECTRÓNICO</label>
-                    <input type="email" />
-                    <label htmlFor="password">CONTRASEÑA</label>
-                    <input type="password" name="" id="" />
-                    <button type="button">Registrarse</button>
-                    <button type="button">Iniciar Sesión</button>
-                </form>
-                <h5>¿Olvidaste tu contraseña?</h5>
+        <div id='Login'>
+            <div className='row'><div className='col-12'>a</div></div>
+            <div className='row cuadrado'>
+                <div className='col-6'>
+                    <img id='imagen' className='img-fluid' src="../../../logoSena.jpeg" alt="SENALOVER" />
+                </div>
+                <div id='form' className='col-6' >
+                    <div className='row' >
+                        <div className='col-2'></div>
+                        <div className='col-8'><h4 className='text-center'>Inicia sesión</h4></div>
+                        <div className='col-2'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <form action="" method="post">
+                                <div id='camposFormulario' className='mb-3'>
+                                    <label className='form-label fw-bold' htmlFor="email" >Correo electrónico:</label>
+                                    <input className='form-control' type="email" placeholder='Ingrese correo'/>
+                                </div>
+                                <div id='camposFormulario' className='mb-3'>
+                                    <label className='form-label fw-bold' htmlFor="password" >Contraseña:</label>
+                                    <input className='form-control' type="password" name="" id="" placeholder='Ingrese contraseña'/>
+                                </div>
+                                
+                                <button className='btn btn-secondary text-dark fw-bold' type="button">Registrarse</button>
+                                <button className='btn btn-dark fw-bold' type="button">Iniciar Sesión</button>
+                            </form>
+                        </div>
+                    </div>
+                    
+                    <h5 className='mt-5 text-center fw-bold'>¿Olvidaste tu contraseña?</h5>
+                    <img className='img-fluid rounded float-end' src="../../../logopequeño.png" alt="logoSena" />
+                </div>
             </div>
         </div>
     )
